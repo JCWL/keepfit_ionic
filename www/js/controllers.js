47d1af51b96782563e7bedf72aedcc1f568f0057
ElizabethMa <ma_yanqiong@163.com>
2015-08-05 17:21:07 +0800

    Signed-off-by: ElizabethMa <ma_yanqiong@163.com> 添加菜单按钮

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,20 +2,161 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('VenuesCtrl', function($scope, loadDataService) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('VenuesCtrl', function($scope, $log, $ionicPopover, loadDataService) {
+  // .fromTemplate() method
+  var template = '<ion-popover-view><ion-header-bar> <h1 class="title">{{title}}</h1> </ion-header-bar> <ion-content> Hello! </ion-content></ion-popover-view>';
+
+  $scope.caluDates = function () {
+    var today = new Date();
+    today.setDate(today.getDate()-1);
+
+    var res = [];
+    var _year='', _month = '', _date = '', _day = '';
+    for (var i = 0; i < 7; i++) {
+        today.setDate(today.getDate()+1);
+        _year = today.getFullYear();
+        _month = today.getMonth()+1;
+        _date = today.getDate();
+        _day = today.getDay();
+        switch (_day)
+        {
+          case 0:
+            _day = '日';
+            break;
+          case 1:
+            _day = '一';
+            break;
+          case 2:
+            _day = '二';
+            break;
+          case 3:
+            _day = '三';
+            break;
+          case 4:
+            _day = '四';
+            break;
+          case 5:
+            _day = '五';
+            break;
+          case 6:
+            _day = '六';
+            break;
+        }
+        var s = _month + '月' + _date + '日 (周' + _day + ')';
+        var ss = _year + '/' + _month + '/' + _date;
+        var obj = {
+          showDate : s,
+          queryDate : ss
+        }
+        res.push(obj);
+    };
+    return res;
+  }
+
+  // 初始化日期数据
+  $scope.dates = $scope.caluDates();
+  $scope.times = [{
+      showTime : '全天(06:00-22:00)',
+      queryTime : '06:00-22:00'
+    },{
+      showTime : '上午(06:00-11:00)',
+      queryTime : '06:00-11:00'
+    },{
+      showTime : '中午(11:00-14:00)',
+      queryTime : '11:00-14:00'
+    },{
+      showTime : '下午(14:00-18:00)',
+      queryTime : '14:00-18:00'
+    },{
+      showTime : '晚上(18:00-22:00)',
+      queryTime : '18:00-22:00'
+    }
+  ];
+
+  // 选中的查询条件，这是默认条件
+  $scope.queryCondition = {
+    showDate : $scope.dates[0].showDate,
+    queryDate : $scope.dates[0].queryDate,
+    showTime : $scope.times[0].showTime,
+    queryTime : $scope.times[0].queryTime,
+    typeName : '',
+    typeId : '',
+    position : '位置'
+  }
+
+  // 选择日期时间时绑定函数
+  $scope.chooseDate = function(showDate, queryDate) {
+      $scope.queryCondition.showDate = showDate;
+      $scope.queryCondition.queryDate = queryDate;
+      $log.log("xuanzele : ", $scope.queryCondition.showDate, $scope.queryCondition.queryDate);
+  }
+
+  $scope.chooseTime = function(showTime, queryTime) {
+      $scope.queryCondition.showTime = showTime;
+      $scope.queryCondition.queryTime = queryTime;
+      $log.log("xuanzele : ", $scope.queryCondition.showTime, $scope.queryCondition.queryTime);
+  }
+
+  // 选择日期时间之后
+  $scope.$on('popoverType.hidden', function() {
+    $log.log("popoverType.hidden");
+  });
+  $scope.$on('popoverDatetime.removed', function() {
+    $log.log("popoverDatetime.removed");
+  });
+
+  // 初始化位置的数据
+  $scope.positions = ['青浦','徐汇','陆家嘴'];
+
+  // 初始化运动类型数据
+  loadDataService.types().success(function(data, status) {
+      $scope.types = data;
+      $scope.queryCondition.typeName = data[0].name;
+      $scope.queryCondition.typeId = data[0].typeId;
+  });
+
+  // 选择种类时绑定函数
+  $scope.chooseType = function(typeName, typeId) {
+      $scope.queryCondition.typeName = typeName;
+      $scope.queryCondition.typeId = typeId;
+      $scope.popoverType.hide();
+      $log.log("xuanzele : ", $scope.queryCondition.typeName, $scope.queryCondition.typeId);
+  }
+
+  // 初始化三个popover
+  $ionicPopover.fromTemplateUrl('templates/popover-datetime.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popoverDatetime = popover;
+  });
+
+  $ionicPopover.fromTemplateUrl('templates/popover-type.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popoverType = popover;
+  });
+
+  $ionicPopover.fromTemplateUrl('templates/popover-position.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popoverPosition = popover;
+  });
+
+  $scope.showpopoverdatetime = function ($event) {
+      $scope.popoverDatetime.show($event);
+  }
+  $scope.showpopovertype = function ($event) {
+      $scope.popoverType.show($event);
+  }
+  $scope.showpopoverposition = function ($event) {
+      $scope.popoverPosition.show($event);
+  }
+
+  // $scope.$on('$ionicView.enter', function(e) {
+  // });
   loadDataService.venueList('ss').success(function (data, status) {
       $scope.venues = data;
   });
-  // $scope.remove = function(chat) {
-  //   Chats.remove(chat);
-  // };
 })
 
 .controller('VenueDetailCtrl', function($scope, $stateParams, loadDataService) {
